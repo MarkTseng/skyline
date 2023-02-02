d36947dfca77b367507de25d9f5f2c785f7b554a
TheASVigilante <65920585+TheASVigilante@users.noreply.github.com>
2023-02-02 21:01:03 +0100

    Optimizations, bugfixes to new swizzling imp

--- app/src/main/cpp/skyline/gpu/texture/layout.cpp
+++ app/src/main/cpp/skyline/gpu/texture/layout.cpp
@@ -4,6 +4,13 @@
 #include "layout.h"
 
 namespace skyline::gpu::texture {
+    #pragma pack(push, 0)
+    struct u96 {
+        u64 high;
+        u32 low;
+    };
+    #pragma pack(pop)
+
     // Reference on Block-linear tiling: https://gist.github.com/PixelyIon/d9c35050af0ef5690566ca9f0965bc32
     constexpr size_t SectorWidth{16}; //!< The width of a sector in bytes
     constexpr size_t SectorHeight{2}; //!< The height of a sector in lines
@@ -93,82 +100,94 @@ namespace skyline::gpu::texture {
      * @tparam BlockLinearToPitch Whether to copy from a blocklinear texture to a pitch-linear texture or a pitch-linear texture to a blocklinear texture
      */
     template<bool BlockLinearToPitch>
-   void CopyBlockLinearInternal(Dimensions dimensions,
-                                size_t formatBlockWidth, size_t formatBlockHeight, size_t formatBpb, u32 pitchAmount,
-                                size_t gobBlockHeight, size_t gobBlockDepth,
-                                u8 *blockLinear, u8 *pitch) {
-       size_t textureWidth{util::DivideCeil<size_t>(dimensions.width, formatBlockWidth)};
-       size_t textureWidthBytes{textureWidth * formatBpb};
-       size_t textureWidthAlignedBytes{util::AlignUp(textureWidthBytes, GobWidth)};
-       size_t GOBsPerRob{textureWidthBytes / GobWidth};
+    void CopyBlockLinearInternal(Dimensions dimensions,
+                                 size_t formatBlockWidth, size_t formatBlockHeight, size_t formatBpb, u32 pitchAmount,
+                                 size_t gobBlockHeight, size_t gobBlockDepth,
+                                 u8 *blockLinear, u8 *pitch) {
+        size_t textureWidth{util::DivideCeil<size_t>(dimensions.width, formatBlockWidth)};
+        size_t textureWidthBytes{textureWidth * formatBpb};
+        size_t textureWidthAlignedBytes{util::AlignUp(textureWidthBytes, GobWidth)};
 
-       if (formatBpb != 12) {
-           while (formatBpb != 16) {
-               if (util::IsAligned(textureWidth, formatBpb * 2)) {
-                   textureWidth /= 2;
-                   formatBpb *= 2;
-               } else {
-                   break;
-               }
-           }
-       }
+        if (formatBpb != 12) {
+            while (formatBpb != 16) {
+                if (util::IsAligned(textureWidth, formatBpb << 1)) {
+                    textureWidth /= 2;
+                    formatBpb <<= 1;
+                } else {
+                    break;
+                }
+            }
+        }
 
-       size_t xPerGOB{GobWidth / formatBpb};
-       size_t xPerLastGOB{(textureWidthBytes - util::AlignDown(textureWidthBytes, GobWidth)) / formatBpb};
+        size_t textureHeight{util::DivideCeil<size_t>(dimensions.height, formatBlockHeight)};
+        size_t robHeight{gobBlockHeight * GobHeight};
 
-       size_t textureHeight{util::DivideCeil<size_t>(dimensions.height, formatBlockHeight)};
-       size_t robHeight{gobBlockHeight * GobHeight};
+        size_t alignedDepth{util::AlignUp(dimensions.depth, gobBlockDepth)};
 
-       size_t alignedDepth{util::AlignUp(dimensions.depth, gobBlockDepth)};
+        size_t pitchBytes{pitchAmount ? pitchAmount : textureWidthBytes};
 
-       size_t pitchBytes{pitchAmount ? pitchAmount : textureWidthBytes};
+        size_t blockSize{robHeight * GobWidth * alignedDepth};
 
-       size_t blockSize{robHeight * GobWidth * alignedDepth};
+        u8 *pitchOffset{pitch};
 
-       u8 *pitchOffset{pitch};
+        auto copyTexture{[&]<typename FORMATBPB>() __attribute((always_inline)) {
+            for (size_t slice{}; slice < dimensions.depth; ++slice, blockLinear += (GobHeight * GobWidth * gobBlockHeight)) {
+                for (size_t line{}; line < textureHeight; ++line, pitchOffset += pitchBytes) {
+                    size_t robOffset{textureWidthAlignedBytes * util::AlignDown(line, robHeight) * alignedDepth};
+                    size_t blockHeight{(line & (robHeight - 1)) / GobHeight};
+                    // Y Offset in entire GOBs
+                    size_t GobYOffset{blockHeight * GobWidth * GobHeight};
+                    // Y Offset inside current GOB
+                    GobYOffset += (((line & 0x07) >> 1) << 6) + ((line & 0x01) << 4);
 
-       for (size_t slice{}; slice < dimensions.depth; ++slice, blockLinear += (GobHeight * GobWidth * gobBlockHeight)) {
-           for (size_t line{}; line < textureHeight; ++line, pitchOffset += pitchBytes) {
-               size_t robOffset{textureWidthAlignedBytes * util::AlignDown(line, robHeight) * alignedDepth};
-               size_t blockHeight{(line % robHeight) / GobHeight};
-               // Y Offset in entire GOBs
-               size_t GobYOffset{blockHeight * GobWidth * GobHeight};
-               // Y Offset inside current GOB
-               GobYOffset += (((line & 0x07) >> 1) << 6) + ((line & 0x01) << 4);
+                    u8 *deSwizzledOffset{pitchOffset};
+                    u8 *swizzledYZOffset{blockLinear + robOffset + GobYOffset};
 
-               u8 *deSwizzledOffset{pitchOffset};
-               u8 *swizzledOffset{blockLinear + robOffset + GobYOffset};
+                    // Copy per every element for the last GOB on X
+                    for (size_t pixel{}; pixel < textureWidth; ++pixel, deSwizzledOffset += formatBpb) {
+                        size_t xBytes{pixel * formatBpb};
+                        size_t blockOffset{(xBytes / GobWidth) * blockSize};
 
-               // Copy per every GOB width
-               for (size_t GOB{}; GOB < GOBsPerRob; ++GOB, deSwizzledOffset += GobWidth, swizzledOffset += blockSize) {
-                   if constexpr (BlockLinearToPitch) {
-                       std::memcpy(deSwizzledOffset, swizzledOffset, SectorWidth);
-                       std::memcpy(deSwizzledOffset + 16, swizzledOffset + 0x20, SectorWidth);
-                       std::memcpy(deSwizzledOffset + 32, swizzledOffset + 0x100, SectorWidth);
-                       std::memcpy(deSwizzledOffset + 48, swizzledOffset + 0x120, SectorWidth);
-                   } else {
-                       std::memcpy(swizzledOffset, deSwizzledOffset, SectorWidth);
-                       std::memcpy(swizzledOffset + 0x20, deSwizzledOffset + 16, SectorWidth);
-                       std::memcpy(swizzledOffset + 0x100, deSwizzledOffset + 32, SectorWidth);
-                       std::memcpy(swizzledOffset + 0x120, deSwizzledOffset + 48, SectorWidth);
-                   }
-               }
+                        // Set offset on X
+                        size_t GobXOffset{(((xBytes & 0x3F) >> 5) << 8) + (xBytes & 0xF) + (((xBytes & 0x1F) >> 4) << 5)};
+                        u8 *swizzledOffset{swizzledYZOffset + blockOffset + GobXOffset};
 
-               // Copy per every element for the last GOB on X
-               for (size_t pixel{textureWidth - xPerLastGOB}; pixel < textureWidth; ++pixel, deSwizzledOffset += formatBpb) {
-                   size_t xBytes{(pixel % xPerGOB) * formatBpb};
+                        if constexpr (BlockLinearToPitch)
+                            *(FORMATBPB *)deSwizzledOffset = *(FORMATBPB *)swizzledOffset;
+                        else
+                            *(FORMATBPB *)swizzledOffset = *(FORMATBPB *)deSwizzledOffset;
+                    }
+                }
+            }
+        }};
 
-                   // Set offset on X
-                   size_t GobXOffset{(((xBytes & 0x3F) >> 5) << 8) + (xBytes & 0xF) + (((xBytes & 0x1F) >> 4) << 5)};
-
-                   if constexpr (BlockLinearToPitch)
-                       std::memcpy(deSwizzledOffset, swizzledOffset + GobXOffset, formatBpb);
-                   else
-                       std::memcpy(swizzledOffset + GobXOffset, deSwizzledOffset, formatBpb);
-               }
-           }
-       }
-   }
+        switch (formatBpb) {
+            case 1: {
+                copyTexture.template operator()<u8>();
+                break;
+            }
+            case 2: {
+                copyTexture.template operator()<u16>();
+                break;
+            }
+            case 4: {
+                copyTexture.template operator()<u32>();
+                break;
+            }
+            case 8: {
+                copyTexture.template operator()<u64>();
+                break;
+            }
+            case 12: {
+                copyTexture.template operator()<u96>();
+                break;
+            }
+            case 16: {
+                copyTexture.template operator()<u128>();
+                break;
+            }
+        }
+    }
 
     /**
      * @brief Copies pixel data between a pitch and part of a blocklinear texture
@@ -190,8 +209,8 @@ namespace skyline::gpu::texture {
         size_t actualOriginX{util::DivideCeil<size_t>(originX, formatBlockWidth)};
         size_t originXBytes{actualOriginX * formatBpb};
 
-        size_t xPerFirstGOB{(GobWidth - (originXBytes % GobWidth)) / formatBpb};
-        size_t xPerLastGOB{((originXBytes + pitchTextureWidthBytes) % GobWidth) / formatBpb};
+        size_t xPerFirstGOB{(GobWidth - (originXBytes & (GobWidth - 1))) / formatBpb};
+        size_t xPerLastGOB{((originXBytes + pitchTextureWidthBytes) & (GobWidth - 1)) / formatBpb};
 
         // Only accounts for GOBs containing padding.
         size_t GOBsPerRob{pitchTextureWidthBytes < GobWidth ? 0 : ((originXBytes + pitchTextureWidthBytes) - util::AlignUp(originXBytes, GobWidth)) / GobWidth};
@@ -205,7 +224,7 @@ namespace skyline::gpu::texture {
 
         size_t pitchBytes{pitchAmount ? pitchAmount : pitchTextureWidthBytes};
 
-        size_t startingBlockOffset{(actualOriginX / GobWidth) * robHeight * GobWidth * alignedDepth};
+        size_t startingBlockOffset{(originXBytes / GobWidth) * robHeight * GobWidth * alignedDepth};
         size_t blockSize{robHeight * GobWidth * alignedDepth};
 
         u8 *pitchOffset{pitch};
@@ -213,7 +232,7 @@ namespace skyline::gpu::texture {
         for (size_t slice{}; slice < blockLinearDimensions.depth; ++slice, blockLinear += (GobHeight * GobWidth * gobBlockHeight)) {
             for (size_t line{}; line < pitchTextureHeight; ++line, pitchOffset += pitchBytes) {
                 u64 robOffset{blockLinearTextureWidthAlignedBytes * util::AlignDown(originYOffset + line, robHeight) * alignedDepth};
-                u64 blockHeight{(originYOffset + line % robHeight) / GobHeight};
+                u64 blockHeight{((originYOffset + line) & (robHeight - 1)) / GobHeight};
                 // Y Offset in entire GOBs
                 u64 GobYOffset{blockHeight * GobWidth * GobHeight};
                 // Y Offset inside 1 GOB
@@ -225,7 +244,7 @@ namespace skyline::gpu::texture {
                 // Copy per every element, don't use for copies larger than GobWidth
                 auto elemCopy{[&](size_t from, size_t to) {
                     for (size_t pixel{from}; pixel < to; ++pixel, deSwizzledOffset += formatBpb) {
-                        size_t xBytes{((actualOriginX + pixel) % xPerGOB) * formatBpb};
+                        size_t xBytes{(actualOriginX + pixel) * formatBpb};
 
                         // Set offset on X
                         size_t GobXOffset{(((xBytes & 0x3F) >> 5) << 8) + (xBytes & 0xF) + (((xBytes & 0x1F) >> 4) << 5)};
